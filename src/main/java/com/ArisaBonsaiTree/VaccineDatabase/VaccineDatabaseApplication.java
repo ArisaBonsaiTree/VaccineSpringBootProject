cdabdf9a7acb802ac366e04692a84593e4bb49e2
ArisaBonsaiTree <angelevarela@hotmail.com>
2022-05-24 18:16:39 -0700

    Creating our First Spring Project

---
+++ src/main/java/com/ArisaBonsaiTree/VaccineDatabase/VaccineDatabaseApplication.java
@@ -0,0 +1,12 @@
+package com.ArisaBonsaiTree.VaccineDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineDatabaseApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(VaccineDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ArisaBonsaiTree/VaccineDatabase/VaccineDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArisaBonsaiTree.VaccineDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccineDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
